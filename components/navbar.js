6086606f3991e71adce5324bff76a1f3c96bcd7f
Anushka Priyadarshi <anushka@Anushkas-MacBook-Air-2.local>
2024-04-08 23:33:14 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    navbar
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,12 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import About from "@/components/about";
-import Donation from "@/components/donation";
-import Exercise from "@/components/exercise";
-import FirstPeriod from "@/components/firstPeriod";
-import MoodAnalysis from "@/components/moodAnalysis";
-import Blogs from "@/components/blogs";
 import Navbar from "@/components/navbar";
+import { Inter } from "next/font/google";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <About/>
-    <Blogs/>
-    <Donation/>
-    <Exercise/>
-    <FirstPeriod/>
-    <MoodAnalysis/>
     <Navbar/>
     </>
   );
